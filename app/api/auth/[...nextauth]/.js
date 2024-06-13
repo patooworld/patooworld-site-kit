5b5aa3f04d3c5dd43bfec93ba9e5b04fb3c87e03
PATOOWORLD <samuel@patooworld.com>
2024-06-13 15:59:27 -0700

    Update and rename route.ts to .js
    change of ...nextauth providers and  import replacement  from nextauth.js

---
+++ app/api/auth/[...nextauth]/.js
@@ -0,0 +1,33 @@
+import NextAuth from "next-auth"
+import NextAuth from 'next-auth'
+import AppleProvider from 'next-auth/providers/apple'
+import FacebookProvider from 'next-auth/providers/facebook'
+import GoogleProvider from 'next-auth/providers/google'
+import EmailProvider from 'next-auth/providers/email'
+import GithubProvider from  'next-auth/providers/github'
+
+  providers: [
+    // OAuth authentication providers...
+    AppleProvider({
+      clientId: process.env.APPLE_ID,
+      clientSecret: process.env.APPLE_SECRET
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_ID,
+      clientSecret: process.env.FACEBOOK_SECRET
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET
+    }),
+    // Passwordless / email sign in
+    EmailProvider({
+      server: process.env.MAIL_SERVER,
+      from: 'NextAuth.js <samuel@example.com>'
+    }),
+    GithubProvider({
+    ClientId: process.env.GITHUB_Client_ID,
+    ClientSecret: process.env.GITHUB_CLIENT_SECRET
+    }),
+  ]
+})
