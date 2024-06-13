633ffa8942f57195883f142e2c316610e55421d4
PATOOWORLD <samuel@patooworld.com>
2024-06-13 16:06:54 -0700

    Update and rename app/api/auth/[...nextauth].js to app/api/auth/[...nextauth]/route.ts
    change from js -> ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,9 @@
 import NextAuth from "next-auth"
-import NextAuth from 'next-auth'
-import AppleProvider from 'next-auth/providers/apple'
-import FacebookProvider from 'next-auth/providers/facebook'
-import GoogleProvider from 'next-auth/providers/google'
-import EmailProvider from 'next-auth/providers/email'
-import GithubProvider from  'next-auth/providers/github'
+import AppleProvider from "next-auth/providers/apple"
+import FacebookProvider from "next-auth/providers/facebook"
+import GoogleProvider from "next-auth/providers/google"
+import EmailProvider from "next-auth/providers/email"
+import GithubProvider from  "next-auth/providers/github"
 
   providers: [
     // OAuth authentication providers...
